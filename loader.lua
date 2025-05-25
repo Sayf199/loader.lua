4bdc4e55c260754e379de6b312f00ce44911150d
Sayf199 <sayfedin.soussi@gmail.com>
2025-05-25 16:39:15 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
-print("[✔️] Script chargé depuis GitHub !")
+local remoteList = {}
+
+for _, v in ipairs(game:GetDescendants()) do
+    if v:IsA("RemoteEvent") or v:IsA("RemoteFunction") then
+        table.insert(remoteList, v:GetFullName())
+    end
+end
+
+print("[📡] Liste des RemoteEvents et RemoteFunctions détectés :")
+for _, remote in ipairs(remoteList) do
+    print("→ " .. remote)
+end
